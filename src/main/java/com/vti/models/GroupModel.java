0ad5ca214b1fdf236e06dd7ff08c1b34633ab685
bobbibao <lehoangbao1125@gmail.com>
2024-04-19 21:09:51 +0700

    Implement GroupModel and UserModel
    - Created relationship many to many between these models

--- src/main/java/com/vti/models/GroupModel.java
+++ src/main/java/com/vti/models/GroupModel.java
@@ -1,10 +1,35 @@
 package com.vti.models;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Table(name = "groups")
+@Getter @Setter @NoArgsConstructor
 public class GroupModel {
 
+	@Id
+	@Column(name = "groupId")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long groupId;
+
+	@Column(name = "groupName")
+	private String groupName;
+	
+	@Column(name = "users")
+	@ManyToMany(mappedBy = "groups", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+	private Set<UserModel> users = new HashSet<UserModel>();
 }
--- src/main/java/com/vti/models/UserModel.java
+++ src/main/java/com/vti/models/UserModel.java
@@ -1,10 +1,46 @@
 package com.vti.models;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Table(name = "users")
+@Getter @Setter @NoArgsConstructor
 public class UserModel {
+	@Id
+	@Column(name = "userId")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long userId;
 
+	@Column(name = "username", nullable = false, unique = true)
+	private String username;
+	
+	@Column(name = "password", nullable = false)
+	private String password;
+
+	@Column(name = "email", nullable = true)
+	private String email;
+	
+	@JsonBackReference
+	@ManyToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+	@JoinTable(name = "group_user", joinColumns = @JoinColumn(name = "userId", referencedColumnName = "userId"),
+			inverseJoinColumns = @JoinColumn(name = "groupId", referencedColumnName = "groupId"))
+	private Set<GroupModel> groups = new HashSet<GroupModel>();
 }
